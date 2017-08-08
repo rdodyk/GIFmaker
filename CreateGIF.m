d6482afe79337ab1bfbef77b59617d36b63a6304
Ryan Dodyk <dodyk@engineering.ualberta.ca>
2017-08-08 12:49:23 -0600

    Works in rudimentary stages now.  Need to refine and polish

--- CreateGIF.m
+++
@@ -1,8 +0,0 @@
-function [ ] = CreateGIF( handles )
-%Create GIF Makes gif from folders full of images
-%   Takes images from folders and combines them into GIF form
-%   Will not work if pronounced "Jiff"
-
-
-end
-
